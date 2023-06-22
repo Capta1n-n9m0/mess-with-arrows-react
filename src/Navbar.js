a2410b4da3e8ac27f3062e40713ede6d9847b679
Abbas Aliyev <alievabbas1@gmail.com>
2023-06-22 14:01:02 +0400

    fix routing

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,10 +6,10 @@ export default function Navbar() {
         <div className="navbar">
             <h1>Navbar</h1>
             <ul className="links">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/cursor-10">Cursor 10</Link></li>
-                <li><Link to="/cursor-50">Cursor 50</Link></li>
-                <li><Link to="/cursor-100">Cursor 100</Link></li>
+                <li><Link to="./">Home</Link></li>
+                <li><Link to="./cursor-10">Cursor 10</Link></li>
+                <li><Link to="./cursor-50">Cursor 50</Link></li>
+                <li><Link to="./cursor-100">Cursor 100</Link></li>
             </ul>
         </div>
     )
